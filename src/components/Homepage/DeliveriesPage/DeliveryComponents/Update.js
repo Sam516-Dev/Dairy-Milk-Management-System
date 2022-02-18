3b841d6e1f5458c5c460878ef86877469093fb75
Sam516-Dev <samuelnjoroge3455@gmail.com>
2022-02-19 02:42:22 +0300

    tommorow
    Signed-off-by: Sam516-Dev <samuelnjoroge3455@gmail.com>

--- src/components/Homepage/DeliveriesPage/DeliveryComponents/Update.js
+++ src/components/Homepage/DeliveriesPage/DeliveryComponents/Update.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react'
 import { Milktitle } from '../../../styled-componets/styles'
 import { useHistory, useLocation } from 'react-router-dom'
 import Axios from 'axios'
+import './Update.css'
 
 const initialData = {
   fullName: '',
@@ -13,40 +14,30 @@ const initialData = {
 function Update() {
   const [State, setState] = useState(initialData)
 
-    const { fullName, quantity, date, farmersID } = initialData
-    
+  const { fullName, quantity, date, farmersID } = initialData
 
-    const handleEditFormSubmit = (event) => {
-        event.preventDefault()
-    }
-    
+  const handleEditFormSubmit = (event) => {
+    event.preventDefault()
+  }
 
-    const handlechange = (e) => {
-        let { name, value } = e.target;
-        setState({...State, [name]: value})
-    }
-    
+  const handlechange = (e) => {
+    let { name, value } = e.target
+    setState({ ...State, [name]: value })
+  }
 
   return (
     <div>
-      <form
-        style={{
-          margin: 'auto',
-          padding: '15px',
-          maxWidth: '400px',
-          alignContent: 'center',
-        }}
-          >
-          <label htmlFor="date"> fullName </label>
-              <input
+      <form>
+        <label htmlFor="date"> fullName </label>
+        <input
           type="text"
           id="fullName"
           name="FullName"
           placeholder="Enter a fullname..."
           onChange={handlechange}
           value={fullName}
-              />
-              <label htmlFor="quantity"> quantity </label>
+        />
+        <label htmlFor="quantity"> quantity </label>
         <input
           type="text"
           id="quantity"
@@ -54,28 +45,27 @@ function Update() {
           placeholder="Enter the quantity..."
           onChange={handlechange}
           value={quantity}
-              />
-              <label htmlFor="date"> date </label>
-              <input
-                type="date"
-                id="date"
-                name="date"
-                placeholder="Enter the date..."
-                onChange={handlechange}
-                value={date}
-              />
-              
-              <label htmlFor="farmersID"> farmersID </label>
-              <input
-                type="farmersID"
-                id="farmersID"
-                name="farmersID"
-                placeholder="Enter the farmersID..."
-                onChange={handlechange}
-                value={farmersID}
-                    />
-      
-      
+        />
+        <label htmlFor="date"> date </label>
+        <input
+          type="date"
+          id="date"
+          name="date"
+          placeholder="Enter the date..."
+          onChange={handlechange}
+          value={date}
+        />
+
+        <label htmlFor="farmersID"> farmersID </label>
+        <input
+          type="farmersID"
+          id="farmersID"
+          name="farmersID"
+          placeholder="Enter the farmersID..."
+          onChange={handlechange}
+          value={farmersID}
+        />
+        <input type="submit" value="Add" />
       </form>
     </div>
   )
